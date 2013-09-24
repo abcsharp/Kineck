f1690fa9ba4bf881ab8088e9604df72bb0a48149
abcsharp <yd.t_kiriu@kpe.biglobe.ne.jp>
2013-09-25 01:30:35 +0900

    add function to bow when the sensor detected a human

--- Kineck/Form1.Designer.cs
+++ Kineck/Form1.Designer.cs
@@ -28,11 +28,12 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(43, 18);
+			this.button1.Location = new System.Drawing.Point(12, 12);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(100, 23);
 			this.button1.TabIndex = 0;
@@ -40,11 +41,22 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(118, 16);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(136, 16);
+			this.checkBox1.TabIndex = 1;
+			this.checkBox1.Text = "人が来たらお辞儀させる";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(186, 59);
+			this.ClientSize = new System.Drawing.Size(261, 47);
+			this.Controls.Add(this.checkBox1);
 			this.Controls.Add(this.button1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
@@ -52,12 +64,14 @@
 			this.Text = "Kineck";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
 
 	}
 }
--- Kineck/Form1.cs
+++ Kineck/Form1.cs
@@ -14,6 +14,8 @@ namespace Kineck
 	public partial class Form1:Form
 	{
 		private KinectSensor sensor;
+		private bool bowing;
+		private Skeleton[] skeletons;
 
 		public Form1()
 		{
@@ -24,13 +26,31 @@ namespace Kineck
 				Application.Exit();
 			}
 			sensor=connectedSensors.First();
+			sensor.SkeletonStream.Enable();
+			sensor.SkeletonFrameReady+=SkeletonFrameReady;
+			skeletons=new Skeleton[sensor.SkeletonStream.FrameSkeletonArrayLength];
 			try{
 				sensor.Start();
 			}catch(Exception){
 				MessageBox.Show("エラーが発生しました。","Kineck",MessageBoxButtons.OK,MessageBoxIcon.Error);
 				Application.Exit();
 			}
+			bowing=false;
 			sensor.ElevationAngle=5;
+		}
+
+		void SkeletonFrameReady(object sender,SkeletonFrameReadyEventArgs e)
+		{
+			using(var skeletonFrame=e.OpenSkeletonFrame()){
+				if(skeletonFrame!=null){
+					skeletonFrame.CopySkeletonDataTo(skeletons);
+					if(skeletons[0].TrackingState==SkeletonTrackingState.Tracked&&!bowing&&checkBox1.Checked){
+						button1.Enabled=false;
+						bowing=true;
+						ThreadPool.QueueUserWorkItem(Bow);
+					}
+				}
+			}
 		}
 
 		private void Form1_FormClosing(object sender,FormClosingEventArgs e)
@@ -40,20 +60,22 @@ namespace Kineck
 
 		private void Bow(object arg)
 		{
-			sensor.ElevationAngle=sensor.MinElevationAngle;
-			Thread.Sleep(250);
 			try{
+				sensor.ElevationAngle=sensor.MinElevationAngle;
+				Thread.Sleep(250);
 				sensor.ElevationAngle=5;
 			}catch(InvalidOperationException){
 				MessageBox.Show("お辞儀に失敗しました。","Kineck",MessageBoxButtons.OK,MessageBoxIcon.Error);
 			}
 			Thread.Sleep(250);
 			Invoke((Action)(()=>button1.Enabled=true));
+			bowing=false;
 		}
 
 		private void button1_Click(object sender,EventArgs e)
 		{
 			button1.Enabled=false;
+			bowing=true;
 			ThreadPool.QueueUserWorkItem(Bow);
 		}
 	}
